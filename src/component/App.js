e677d3a1274f90f2ea28d424ce54e644e79029ad
Ayush Kaushik <kaushika@uoguelph.ca>
2020-08-15 16:54:51 -0400

    PAGE: Updated icon and title of application

--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import GameView from '../layout/GameView';
 import {ThemeContextProvider} from '../context/themeContext';
 import "../App.css";
 
 function App() {
+    useEffect(() => {
+        document.title="Roll Dice"
+    }, []);
+
     return (
         <ThemeContextProvider>
             <GameView/>
